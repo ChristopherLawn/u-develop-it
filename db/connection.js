7b97919ff8e5bc094ad02874ac45b40bd68ae972
CLAW <christopher.lawn@yahoo.com>
2022-01-26 01:08:11 -0800

    updated connection.js file

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your My SQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Ru$$1anV0dka',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+
+module.exports = db;
